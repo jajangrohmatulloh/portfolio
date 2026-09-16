06ac9fcd6955c1e75e44661f3c113f53df95a827
Jajang Rohmatulloh <jajang.rohmatulloh02@gmail.com>
2026-09-16 10:06:40 +0700

    Update role description for System Engineer L1

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -15,7 +15,7 @@ const experiences = [
   },
   {
     company: "Global Infotech Solution",
-    role: "System Engineer L1 (Handling Full Stack & Salesforce Development)",
+    role: "System Engineer L1 (Handled Full Stack Development)",
     period: "Feb 2023 – May 2024 • On-site",
     description: "Built a mobile and web application using React Native, React, and Node.js. Customized and developed a Salesforce application and integrated Google Drive, reducing manual processes by 15%. Earned 5+ certifications (Salesforce Administrator, Salesforce Developer, and MuleSoft Developer) and updated technical documentation for client-facing projects.",
   },
